3b006ab9987279d44b74596a7fdabeea8c904e29
AkshayGets <40195161+AkshayGets@users.noreply.github.com>
2025-05-20 19:55:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,7 +83,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 
   rule {
     apply_server_side_encryption_by_default {
-      sse_algorithm = "aws:kms"
+      sse_algorithm = "AES256"
     }
   }
 }
